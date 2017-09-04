6e1d0c175d780d6abe510d6238050586524c32dc
george-x-jiang <george_x_jiang@yahoo.com>
2017-09-04 22:48:01 +1000

    Smoke test production build

--- local-api-server.js
+++ local-api-server.js
@@ -21,6 +21,29 @@ app.use('/useradmin-service/profile/queryPostcode',
 
 app.use('/useradmin-service', localApi)
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(app.get('port'), function() {
   console.log('eCompany SPA running on port', app.get('port'));
